460fbf05aaca3fb77b3d98e76ff403b9a938e6bb
kotechasitz <fewzanpk@gmail.com>
2019-05-18 01:18:45 +0700

    Initial commit

---
+++ src/loansystem/UserLogin.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loansystem;
+
+/**
+ *
+ * @author Kannawat
+ */
+public class UserLogin {
+    private final String name = "test";
+    private final String password = "123";
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+}
